fa0fb2da5ac16074c3686d803798843aa6a08af9
geoffserv <gtraugott@gmail.com>
2021-09-30 10:22:05 -0500

    Spinning animation

--- gfxTest.py
+++ gfxTest.py
@@ -8,76 +8,186 @@ import math
 import pygame # brew install pygame
 from pygame.locals import *
 
-class triangleSlice:
-	def __init__(self, r, sliceNo=0):
-		self.sliceNo = sliceNo
-		self.r = r
-		triangleCoords = []
-		triangleCoords.append( # Origin
+class shapeWheel(object):
+	def __init__(self, r, sliceNo=1, offsetDegs=0, canvasMargin=10, labelList=[]):
+		self.sliceNo = int(sliceNo)
+		self.r = int(r)
+		self.circleDivisions = 12 # 12-slices around the circle
+		self.offsetDegs = int(offsetDegs)
+		self.canvasMargin = int(canvasMargin)
+		self.originX = (self.r + self.canvasMargin) # Center of the canvas X
+		self.originY = (self.r + self.canvasMargin) # Center of the canvas Y
+		self.canvasWidth = ((self.r * 2) + (self.canvasMargin * 2))
+		self.canvasHeight = ((self.r * 2) + (self.canvasMargin * 2))
+		self.labelList = labelList
+
+		self.coords = []
+
+		self.findCoords()
+
+	def findCoords(self):
+		for i in range(12):
+			self.coords.append( # One corner of the triangle along the circle radius r, at sliceNo*1/12circle degs
+				(
+					(
+						self.originX +
+						int(1 * self.r * math.sin(
+								math.radians(
+									((360/self.circleDivisions) * i) + self.offsetDegs
+								)
+							)
+						)
+					),
+					(
+						self.originY +
+						int(1 * self.r * math.cos(
+								math.radians(
+									((360/self.circleDivisions) * i) + self.offsetDegs
+								)
+							)
+						)
+					)
+				)
+			)
+
+class shapeWheelTriangleSlice(shapeWheel):
+	def findCoords(self):
+		self.coords.append( # Origin
 			(
-				(self.wheelr +
-				 self.canvasMargin),
-				(self.wheelr +
-				 self.canvasMargin)
+				self.originX,
+				self.originY
 			)
 		)
-		triangleCoords.append( #
+		self.coords.append( # One corner of the triangle along the circle radius r, at sliceNo*1/12circle degs
 			(
 				(
-					self.wheelr +
-					self.canvasMargin +
-					int(1 * self.wheelr * math.sin(
+					self.originX +
+					int(1 * self.r * math.sin(
 							math.radians(
-								(self.twelvthOfCircleDegs * i) + self.twentyFourthOfCircleDegs
+								((360/self.circleDivisions) * self.sliceNo) + self.offsetDegs
 							)
 						)
 					)
 				),
 				(
-					self.wheelr +
-					self.canvasMargin +
-					int(1 * self.wheelr * math.cos(
+					self.originY +
+					int(1 * self.r * math.cos(
 							math.radians(
-								(self.twelvthOfCircleDegs * i) + self.twentyFourthOfCircleDegs
+								((360/self.circleDivisions) * self.sliceNo) + self.offsetDegs
 							)
 						)
 					)
 				)
 			)
 		)
-		print(guideCircle)
-		renderposition = [0, 0]
+		self.coords.append(  # One corner of the triangle along the circle radius r, at (sliceNo+1)*1/12circle degs
+			(
+				(
+						self.originX +
+						int(1 * self.r * math.sin(
+							math.radians(
+								((360 / self.circleDivisions) * (self.sliceNo+1)) + self.offsetDegs
+							)
+						)
+							)
+				),
+				(
+						self.originY +
+						int(1 * self.r * math.cos(
+							math.radians(
+								((360 / self.circleDivisions) * (self.sliceNo+1)) + self.offsetDegs
+							)
+						)
+							)
+				)
+			)
+		)
 
-		polygon = pygame.Surface((self.wheelCanvas, self.wheelCanvas))
-		pygame.draw.polygon(polygon, self.orange, guideCircle, 1)
+class controlSystem(object):
+	def __init__(self, canvasWidth, canvasHeight, canvasMargin, color, notes, blitX, blitY):
+		self.surface = None
+		self.canvasWidth = canvasWidth
+		self.canvasHeight = canvasHeight
+		self.canvasMargin = canvasMargin
+		self.color = color
+		self.notes = notes # Holds the dict/lists representing the scale notes, their positions and values, etc
+		self.blitX = blitX # The X location in which this entire control should be blit to the screen canvas
+		self.blitY = blitY # The Y location in which this entire control should be blit to the screen canvas
+		self.surface = None
 
+		self.refreshSurface()
 
+	def initSurface(self):
+		pass
+
+	def refreshSurface(self):
+		self.surface = pygame.Surface((int(self.canvasWidth + (self.canvasMargin * 2)),
+									   int(self.canvasHeight + (self.canvasMargin * 2))))
+
+	def drawPolygon(self, shape):
+		pygame.draw.polygon(self.surface, self.color, shape.coords, 1)
+
+	def drawControl(self):
+		pass
+
+	def updateControl(self):
+		pass
+
+class ffWheel(controlSystem):
+	def initSurface(self):
+		# ffWheel-specific object attributes
+		self.r = int(self.canvasHeight/2)
+		self.rotateOffset = 0
+		self.rotateSteps = 0
+		self.offsetDegs = int(360/24) # Control offset degrees
+
+	def updateControl(self):
+		if self.rotateSteps > 0:
+			self.rotateOffset += 1
+			self.rotateSteps -= 1
+
+	def drawControl(self):
+		# Draw the reference circle
+		polygon = shapeWheel(self.r,
+							 sliceNo=1,
+							 offsetDegs=self.offsetDegs + self.rotateOffset,
+							 canvasMargin=self.canvasMargin)
+		self.drawPolygon(polygon)
+
+		# Draw the slices
+		for i in range(12):
+			polygon = shapeWheelTriangleSlice(self.r,
+											  sliceNo=i,
+											  offsetDegs=self.offsetDegs + self.rotateOffset,
+											  canvasMargin=self.canvasMargin)
+			self.drawPolygon(polygon)
 
 class fourtyFive:
-	def __init__(self, canvasWidth=1028, canvasHeight=720):
-		pygame.init()
-		self.canvas = pygame.display.set_mode([canvasWidth,canvasHeight]) # 16x9 SD for later capture in to VDMX
-		pygame.display.set_caption('fourtyFive') # Set the window title for fun
+	def __init__(self, canvasWidth=1920, canvasHeight=1080):
 
-		self.width = canvasWidth
-		self.height = canvasHeight
-		self.centerX = self.canvas.get_rect().centerx
-		self.centerY = self.canvas.get_rect().centery
-		self.bottom  = self.canvas.get_rect().bottom
+		# Musical attributes
+		self.notes = [
+			{'noteName': 'c', 'kbNum': 1, 'wheelPos': 1},
+			{'noteName': 'g', 'kbNum': 8, 'wheelPos': 2},
+			{'noteName': 'd', 'kbNum': 3, 'wheelPos': 3},
+			{'noteName': 'a', 'kbNum': 10, 'wheelPos': 4},
+			{'noteName': 'e', 'kbNum': 5, 'wheelPos': 5},
+			{'noteName': 'b', 'kbNum': 12, 'wheelPos': 6},
+			{'noteName': 'gb/f#', 'kbNum': 7, 'wheelPos': 7},
+			{'noteName': 'db/c#', 'kbNum': 2, 'wheelPos': 8},
+			{'noteName': 'ab/g#', 'kbNum': 9, 'wheelPos': 9},
+			{'noteName': 'eb/d#', 'kbNum': 4, 'wheelPos': 10},
+			{'noteName': 'bb/a#', 'kbNum': 11, 'wheelPos': 11},
+			{'noteName': 'f', 'kbNum': 6, 'wheelPos': 12}
+		]
 
-		self.twelvthOfCircleDegs = int(360 / 12)
-		self.twentyFourthOfCircleDegs = int(360 / 24)
+		# Graphics attributes
 
-		self.wheelr = int((canvasHeight * .8)/2) # R is half of __% of the screen
+		self.canvasWidth = canvasWidth
+		self.canvasHeight = canvasHeight
+
+		self.r = int((canvasHeight * .8)/2) # R is half of __% of the screen
 		self.canvasMargin = 10
-		self.wheelCanvas = int((self.wheelr * 2) + (self.canvasMargin * 2)) # Add the margin all around
-
-		print("DEBUG -- screen_width:", self.width)
-		print("DEBUG -- screen_height:", self.height)
-		print("DEBUG -- screen_bottom:", self.bottom)
-		print("DEBUG -- screen_centerX:", self.centerX)
-		print("DEBUG -- screen_centerY:", self.centerY)
-		print("DEBUG -- screen_bottom:", self.bottom)
 
 		# Define some colors for convenience and readability
 		self.black = (0,0,0)
@@ -87,47 +197,59 @@ class fourtyFive:
 
 		self.running = False # will be True once self.run() is called
 
-		self.run()
+		# Initialize the canvas
+		pygame.init()
+		self.canvas = pygame.display.set_mode([self.canvasWidth, self.canvasHeight])
+		pygame.display.set_caption('fourtyFive')  # Set the window title for fun
+
+		self.centerX = self.canvas.get_rect().centerx
+		self.centerY = self.canvas.get_rect().centery
+		self.bottom  = self.canvas.get_rect().bottom
+
+		# controlSurfaces list contains each controlSystem object that is rendered.
+		# Declare controlSystem objects, set them up and init them, then append() them
+		# to this list.  Then each in turn will get a drawControl() call and their
+		# surface attribute will be blit to the cancas.
+		self.controlSurfaces = []
+
+		# The size of the ffWheel's surface will be __% of the screen
+		controlFfWheelSize = int(self.canvasHeight * 0.8)
+		# Create a ffWheel control.  Init.
+		controlFfWheel = ffWheel(controlFfWheelSize, controlFfWheelSize,  # Size of this control's surface.
+								 self.canvasMargin, self.orange, self.notes,  # Margin, render color, list of note vals
+								 int((self.canvasWidth / 2) - (controlFfWheelSize/2)), 0)  # Blit location on the screen
+		controlFfWheel.initSurface()
+		# Rotate 360?
+		controlFfWheel.rotateSteps = 360
+		# Append the ffWheel to the controlSurfaces list
+		self.controlSurfaces.append(controlFfWheel)
 
 	def run(self):
 		self.running = True
-		guideCircle = []
-		for i in range(12):
-			guideCircle.append(
-				(self.wheelr +
-				 self.canvasMargin +
-				 int(1 * self.wheelr * math.sin(
-					                   		math.radians(
-												(self.twelvthOfCircleDegs * i) + self.twentyFourthOfCircleDegs
-											)
-				 						)
-					 ),
-				 self.wheelr +
-				 self.canvasMargin +
-				 int(1 * self.wheelr * math.cos(
-											 math.radians(
-												 (self.twelvthOfCircleDegs * i) + self.twentyFourthOfCircleDegs
-											 )
-										 )
-					 )
-				 )
-		 	)
-		print(guideCircle)
-		renderposition = [0, 0]
 
-		polygon = pygame.Surface((self.wheelCanvas, self.wheelCanvas))
-		pygame.draw.polygon(polygon, self.orange, guideCircle, 1)
-
+		# The main running loop
 		while self.running:
-			self.canvas.fill(self.black)  # fill the screen with black, obliterating everything
+			# Draw the controls
+			self.canvas.fill(self.black)  # fill the screen with black
 
-			self.canvas.blit(polygon, renderposition)
+			# Loop through each controlSystem added to the controlSurfaces list
+			for controlSurface in self.controlSurfaces:
+				# The drawControl method should update the control's visual elements and
+				# draw to the control's surface
+				controlSurface.drawControl()
+				# Blit the control's surface to the canvas
+				self.canvas.blit(controlSurface.surface, [controlSurface.blitX, controlSurface.blitY])
 
 			pygame.display.update()
 
+			# Update controls
 			for event in pygame.event.get():
 				if event.type == QUIT:  # If the window 'close' button is clicked...
 					self.running = False
+
+			for controlSurface in self.controlSurfaces:
+				controlSurface.refreshSurface() # clear the underlying surfaces for the next draw
+				controlSurface.updateControl() # update control attributes based on what needs to happen
 
 		pygame.quit()
 
@@ -135,3 +257,4 @@ class fourtyFive:
 
 if __name__ == "__main__":
 	fourtyFive = fourtyFive()
+	fourtyFive.run()
